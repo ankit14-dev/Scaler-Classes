f7cd39c7f9de35ef40d95f4ada5e574e628aae5b
Ankit Kumar <ankitkr.ak14@gmail.com>
2024-08-22 09:43:30 +0530

    web dev class 4 commited

---
+++ ICP/javaCode/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class HelloWorld{
+//main function: program execution starts here
+	public static void main(String args[]){
+		System.out.print(100+100+"100"+100+100+100);
+	
+	}
+
+}
